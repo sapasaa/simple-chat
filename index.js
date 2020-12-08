f6dbc7da85c210ff96887b362345794434d2efc9
Sa Pasa <sa.pasa@fdmgroup.com>
2020-12-08 16:41:35 +1100

    adding a start script may fix

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const connectMongoDB = require('./config');
+
 connectMongoDB();
 app.use(express.json({ extented: false, limit: '50mb'}));
-app.get('/', (req,res) => {
-    res.send("Hello World")
-});
 
 app.use("/api/post", require("./route/post"));
 app.use("/api/feed", require("./route/feed"));
+
+app.use(express.static('client/build'));
+app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
