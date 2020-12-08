2bf353ec2c74ef234bf27576c81c14751ccaf618
Sa Pasa <sa.pasa@fdmgroup.com>
2020-12-08 11:48:23 +1100

    server runs

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const app = express();
+
+app.use(express.json({ extented: false, limit: '50mb'}));
+
+app.get('/', (req, res)=>{
+    res.send("Hello World")
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
